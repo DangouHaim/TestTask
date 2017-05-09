97eafd1c955e31d8354eaf9d2d95dbc00cee16a5
DangouHaim <gholub1996@mail.ru>
2017-05-09 14:00:54 +0300

    Update style.js

--- js/style.js
+++ js/style.js
@@ -1,3 +1,3 @@
 $(document).ready(function(){
-	$("head").append('<link rel="stylesheet" href="libs/bootstrap/bootstrap-grid-3.3.2.min.css" /><link rel="stylesheet" href="libs/FontOwesume/css/font-awesome.min.css" /><link rel="stylesheet" href="libs/owl/assets/owl.carousel.css" /><link rel="stylesheet" href="libs/mmenu/dist/jquery.mmenu.all.css" /><link rel="stylesheet" href="css/fonts.css" /><link rel="stylesheet" href="css/main.css" /><link rel="stylesheet" href="css/media.css" />');
+	$("head").append('<link rel="stylesheet" href="libs/FontOwesume/css/font-awesome.min.css" /><link rel="stylesheet" href="libs/owl/assets/owl.carousel.css" /><link rel="stylesheet" href="libs/mmenu/dist/jquery.mmenu.all.css" /><link rel="stylesheet" href="css/fonts.css" /><link rel="stylesheet" href="css/media.css" />');
 });
